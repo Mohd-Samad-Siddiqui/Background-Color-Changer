e36e001d7de4331a5e44e5190bc127ba9d820385
MOHD SAMAD SIDDIQUI <samad.siddiqui47@gmail.com>
2024-01-12 19:40:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,32 +6,16 @@ const bodybg = document.querySelector('body');
 
 firstColor.addEventListener('click', () => {
   bodybg.style.backgroundColor = "#D98880";
-  firstColor.style.border = '2px solid #fff';
-  secondColor.style.border = 'none';
-  thirdColor.style.border = 'none';
-  fourthColor.style.border = 'none';
 })
 
 secondColor.addEventListener('click', () => {
   bodybg.style.backgroundColor = "#A9DFBF";
-  secondColor.style.border = '2px solid #fff';
-  firstColor.style.border = 'none';
-  thirdColor.style.border = 'none';
-  fourthColor.style.border = 'none';
 })
 
 thirdColor.addEventListener('click', () => {
   bodybg.style.backgroundColor = "#5DADE2";
-  thirdColor.style.border = '2px solid #fff';
-  firstColor.style.border = 'none';
-  secondColor.style.border = 'none';
-  fourthColor.style.border = 'none';
 })
 
 fourthColor.addEventListener('click', () => {
   bodybg.style.backgroundColor = "#CCD1D1";
-  fourthColor.style.border = '2px solid #fff';
-  firstColor.style.border = 'none';
-  secondColor.style.border = 'none';
-  thirdColor.style.border = 'none';
 })
